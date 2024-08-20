92f841ff1f22318b1990ec935c4580419b50fa49
Mamun0707 <abdulalmamun637@gmail.com>
2024-08-20 10:44:37 +0600

    image

---
+++ product.php
@@ -0,0 +1,32 @@
+<?php include('include/header.php') ; ?>
+
+  <!-- about section -->
+  <section class="about_section layout_padding">
+    <div class="container">
+      <div class="custom_heading-container ">
+        <h2>
+          NAPA EXTEND
+        </h2>
+      </div>
+
+      <div class="img-box">
+        <img src="images/napa-extend.png" alt="" height="500">
+      </div>
+      <div class="detail-box">
+        <p>
+         Napa Extend Tablet contains Paracetamol, which is classified as an Analgesic and Antipyretic medication. It is commonly used to relieve mild to moderate pain, such as headaches, backaches, toothaches, severe nerve pain, sore throat, period pain, low back and leg pain, muscle, connective tissue, and joint pain. It can also help reduce fever and alleviate symptoms associated with cold or flu.
+        </p>
+        <div class="d-flex justify-content-center">
+          <a href="">
+            Price : $2 
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!-- Footer -->
+            
+            <?php include('include/footer.php') ; ?>
+
+  
