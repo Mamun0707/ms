62874056f1c632d0094976add21b7c614161acb8
NVIT <shariful0606@gmail.com>
2024-08-21 12:46:27 +0600

    update sslcmrz link

--- sslcmz/config/config.php
+++ sslcmz/config/config.php
@@ -1,7 +1,7 @@
 <?php
-
+require(__DIR__ . '/../../include/connection.php');
 if (!defined('PROJECT_PATH')) {
-    define('PROJECT_PATH', 'http://localhost/ms'); // replace this value with your project path
+    define('PROJECT_PATH', $baseurl); // replace this value with your project path
 }
 
 if (!defined('IS_SANDBOX')) {
